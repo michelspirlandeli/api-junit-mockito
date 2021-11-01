3ca83c2e26d57c9e0b40dd20aa61d77c8be09e72
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-01 16:03:22 -0300

    :rocket:nova feature - interface user repository

---
+++ src/main/java/com/michelspirlandeli/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.michelspirlandeli.repositories;
+
+import com.michelspirlandeli.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
