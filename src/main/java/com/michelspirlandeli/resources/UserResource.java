a26526fa61639e8fefcb7e6c2731958ad949872f
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-01 16:04:44 -0300

    :rocket:nova feature - resources findById

--- src/main/java/com/michelspirlandeli/resources/UserResource.java
+++ src/main/java/com/michelspirlandeli/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.michelspirlandeli.resources;
 
 import com.michelspirlandeli.domain.User;
+import com.michelspirlandeli.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1,"michel", "michel@gmail.com", "123456"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
