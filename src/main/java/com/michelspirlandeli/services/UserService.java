0816c4557594ac054e4b891d43582ebc41a4e7bb
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-02 09:59:14 -0300

    :rocket:nova feature - endpoint findAll

--- src/main/java/com/michelspirlandeli/services/UserService.java
+++ src/main/java/com/michelspirlandeli/services/UserService.java
@@ -2,6 +2,9 @@ package com.michelspirlandeli.services;
 
 import com.michelspirlandeli.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/com/michelspirlandeli/services/impl/UserServiceImpl.java
+++ src/main/java/com/michelspirlandeli/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.michelspirlandeli.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
