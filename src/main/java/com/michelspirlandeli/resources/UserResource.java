6a2c9c46e3784639a70383c8c0597fcad7dc98cd
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-02 09:51:08 -0300

    :rocket:nova feature - return dto

--- src/main/java/com/michelspirlandeli/resources/UserResource.java
+++ src/main/java/com/michelspirlandeli/resources/UserResource.java
@@ -1,7 +1,8 @@
 package com.michelspirlandeli.resources;
 
-import com.michelspirlandeli.domain.User;
+import com.michelspirlandeli.domain.dto.UserDTO;
 import com.michelspirlandeli.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
