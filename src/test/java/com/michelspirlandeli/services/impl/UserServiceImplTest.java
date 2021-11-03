181deb5fc0396bcdcf2a986c400a8e5626fdfed8
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-03 14:11:32 -0300

    :rocket:nova feature - test class userServiceImpl method findById

---
+++ src/test/java/com/michelspirlandeli/services/impl/UserServiceImplTest.java
@@ -0,0 +1,87 @@
+package com.michelspirlandeli.services.impl;
+
+import com.michelspirlandeli.domain.User;
+import com.michelspirlandeli.domain.dto.UserDTO;
+import com.michelspirlandeli.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+class UserServiceImplTest {
+
+    private static final Integer ID      = 1;
+    private static final Integer INDEX   = 0;
+    private static final String NAME     = "Valdir";
+    private static final String EMAIL    = "valdir@mail.com";
+    private static final String PASSWORD = "123";
+
+    private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    private static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL,  PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
