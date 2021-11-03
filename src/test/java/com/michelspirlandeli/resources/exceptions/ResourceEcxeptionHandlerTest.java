b9317ebe502df1f0c6abff398c4960aefe0daf08
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-03 17:35:44 -0300

    :rocket:nova feature - test class userResourceException dataIntegrityViolationException

--- src/test/java/com/michelspirlandeli/resources/exceptions/ResourceEcxeptionHandlerTest.java
+++ src/test/java/com/michelspirlandeli/resources/exceptions/ResourceEcxeptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.michelspirlandeli.resources.exceptions;
 
+import com.michelspirlandeli.services.exceptions.DataIntegratyViolationException;
 import com.michelspirlandeli.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResourceEcxeptionHandlerTest {
 
     private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    private static final String E_MAIL_JA_CADASTRADO = "E-mail já cadastrado";
 
     @InjectMocks
     private ResourceEcxeptionHandler ecxeptionHandler;
@@ -47,5 +49,17 @@ class ResourceEcxeptionHandlerTest {
 
     @Test
     void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = ecxeptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException(E_MAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
