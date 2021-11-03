95d354259a4f67f84b078e579ce01ff94bf910e3
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-03 16:32:24 -0300

    :rocket:nova feature - test class userServiceImpl method delete success

--- src/test/java/com/michelspirlandeli/services/impl/UserServiceImplTest.java
+++ src/test/java/com/michelspirlandeli/services/impl/UserServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -149,7 +149,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
 
