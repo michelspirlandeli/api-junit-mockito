5fcfef08259afec76fba01be59e64408fded5826
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-03 16:20:12 -0300

    :rocket:nova feature - test class userServiceImpl method create witch exception

--- src/test/java/com/michelspirlandeli/services/impl/UserServiceImplTest.java
+++ src/test/java/com/michelspirlandeli/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.michelspirlandeli.services.impl;
 import com.michelspirlandeli.domain.User;
 import com.michelspirlandeli.domain.dto.UserDTO;
 import com.michelspirlandeli.repositories.UserRepository;
+import com.michelspirlandeli.services.exceptions.DataIntegratyViolationException;
 import com.michelspirlandeli.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -105,6 +106,19 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
         assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenCreateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try{
+            optionalUser.get().setId(2);
+            service.create(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, ex.getMessage());
+        }
     }
 
     @Test
